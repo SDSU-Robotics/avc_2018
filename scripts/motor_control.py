6fbacaae8ffe1643d81f02eec4521dddfafa9d66
Joel Quanbeck <jqquanbeck@gmail.com>
2018-08-17 19:22:29 -0500

    Fixed many bugs including crashing

--- scripts/motor_control.py
+++ scripts/motor_control.py
@@ -5,60 +5,65 @@ from std_msgs.msg import Float64
 import math
 import serial
 
- 
+
 # Motor labeling convention:
- 
+
 #    Front
 #   0     1
-   
+
 #   2     3
-   
+
 #   4     5
- 
+
 MOTOR_QUANTITY = 6
 PWM_PINS = [26, 18, 20, 21, 22, 23]
-DIR_PINS = [5, 6, 16, 19, 12, 13]
-
-class Listener:
-    motors = []
-    def __init__(self, pi):
-        for i in range (0, MOTOR_QUANTITY):
-            self.motors.append(Motor(pi, PWM_PINS[i], DIR_PINS[i]))
-
-    def lCallback(self, l_speed):
-            for i in range (0, 6, 2):
-                self.motors[i].setSpeed(l_speed)
-
-    def rCallback(self, r_speed):
-            for i in range (1, 6, 2):
-                self.motors[i].setSpeed(r_speed)
+DIR_PINS = [5, 7, 16, 19, 12, 13]
 
 class Motor:
-    def __init__(self, pi, pwm, dir):
+    def __init__(self, pi, pwm, dirPin):
         self.pi = pi
         self.pwm = pwm
-        self.dir = dir
+        self.dirPin = dirPin
         self.pi.set_mode(self.pwm, pigpio.OUTPUT)
-        self.pi.set_mode(self.dir, pigpio.OUTPUT)
+        self.pi.set_mode(self.dirPin, pigpio.OUTPUT)
 
     def setSpeed(self, speed):
+        speed = speed * 255
 
-        if speed.data > 0:
-            self.pi.write(self.dir, 1)
+        if speed > 0:
+            self.pi.write(self.dirPin, 1)
 
         else:
-            self.pi.write(self.dir, 0)
-            speed.data = -speed.data
+            self.pi.write(self.dirPin, 0)
+            speed = -speed
 
-        print(self.pi.read(16))
-        rospy.loginfo("PWM Val: %f", self.dir)
-        self.pi.set_PWM_dutycycle(self.pwm, speed.data * 255.0)
+        if speed > 255:
+            speed = 255
+
+        rospy.loginfo("PWM Val: %f  Dir: %d  PWM %d", speed, self.dirPin, self.pwm)
+        self.pi.set_PWM_dutycycle(self.pwm, speed)
+
+class Listener:
+    def __init__(self, pi):
+        self.motors = []
+        for i in range(0, MOTOR_QUANTITY):
+            self.motors.append(Motor(pi, PWM_PINS[i], DIR_PINS[i]))
+            self.motors[i].setSpeed(0)
+
+    def lCallback(self, l_speed):
+        for i in range(0, MOTOR_QUANTITY, 2):
+            self.motors[i].setSpeed(l_speed.data)
+
+    def rCallback(self, r_speed):
+        for i in range(1, MOTOR_QUANTITY, 2):
+            self.motors[i].setSpeed(r_speed.data)
 
 def motor_control():
     rospy.init_node('motor_control', anonymous = True)
+
     pi = pigpio.pi()
     if pi < 0:
-        ROS_INFO("Error: ", pigpio_error(pi))
+        rospy.logerr("Error: ", pigpio_error(pi))
 
     listener = Listener(pi)
 
