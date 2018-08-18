869d9e665c193446ae509fb49705387f5e841fc4
Joel Quanbeck <jqquanbeck@gmail.com>
2018-08-17 21:49:44 -0500

    Changed to available pins for emag

--- scripts/constants.py
+++ scripts/constants.py
@@ -7,7 +7,7 @@ LIDAR2_PORT = "/dev/ttyUSB2"
 XBEE_PORT = "/dev/ttyUSB3"
 
 WHEEL_PWM_PINS = [26, 18, 20, 21, 22, 23]
-WHEEL_DIR_PINS = [5, 6, 16, 19, 12, 13]
+WHEEL_DIR_PINS = [5, 7, 16, 19, 12, 13]
 
-EMAG_FIN_PIN = 27 #arbitrary
-EMGA_RIN_PIN = 28 #arbitrary
+EMAG_FIN_PIN = 8 #arbitrary
+EMGA_RIN_PIN = 24 #arbitrary
