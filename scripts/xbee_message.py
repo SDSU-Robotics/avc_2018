3ccd1f878ec0902d408986bc419241e124e9c683
Joel Kocer <joel.kocer@jacks.sdstate.edu>
2018-09-03 04:44:45 -0500

    Updated xbee_message to better handle serial errors

--- scripts/xbee_message.py
+++ scripts/xbee_message.py
@@ -4,39 +4,51 @@ from xbee import XBee, ZigBee
 import serial
 import rospy
 from std_msgs.msg import String
+import constants
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+#ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 
-
-# Use an XBee 802.15.4 device
-xbee = XBee(ser)
-# To use with an XBee ZigBee device, replace with:
-# xbee = ZigBee(ser)
 
 
 def xbee_message():
-
     xbee_msg = rospy.Publisher('xbee_message', String, queue_size=10)
-    rospy.init_node('xbee', anonymous=True)
-    rate = rospy.Rate(10)
+    rospy.init_node('xbee', anonymous=False)
+    rate = rospy.Rate(1000)
+
+    # try to open serial ports
+    try: ser = serial.Serial(constants.XBEE_PORT, 115200, timeout = .1)
+    except: rospy.logerr("Failed to open %s", constants.XBEE_PORT)    
+
+    i = 0
 
     while not rospy.is_shutdown():
+        if ser != 0:
+            #read xbee data
 
-            msg = xbee.wait_read_frame()
-            rospy.loginfo(msg['rf_data'])
-            xbee_msg.publish(msg['rf_data'])
-            rate.sleep()
+            # Use an XBee 802.15.4 device
+            xbee = XBee(ser)
+            # To use with an XBee ZigBee device, replace with:
+            # xbee = ZigBee(ser)
+            msg = xbee.wait_read_frame()   
+ 
+            i = i + 1
+            print(i)
 
+            #rospy.loginfo(msg['rf_data'])
+            #xbee_msg.publish(msg['rf_data'])
+        
+    rate.sleep()
 
+       
 
 if __name__ =='__main__':
     try:
         xbee_message()
     except rospy.ROSInterruptException:
-        pass
+        ser.close()
 
-ser.close()
+
 
 
 
